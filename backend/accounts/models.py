d7eb5e8f9f7d94f65b11b08e4a1bdc8453bdcbe1
sahidul islam <sahidafridi7099@gmail.com>
2026-06-19 22:56:48 +0530

    Add custom user model

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,3 +1,20 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    class Role(models.TextChoices):
+        ADMIN = "ADMIN", "Admin"
+        TEACHER = "TEACHER", "Teacher"
+        STUDENT = "STUDENT", "Student"
+
+    role = models.CharField(
+        max_length=20,
+        choices=Role.choices,
+        default=Role.STUDENT
+    )
+
+    phone = models.CharField(max_length=15, blank=True)
+
+    def __str__(self):
+        return f"{self.username} ({self.role})"
--- backend/config/settings.py
+++ backend/config/settings.py
@@ -37,7 +37,20 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'corsheaders',
+
+    'accounts',
+    'students',
+    'teachers',
+    'subjects',
+    'tests',
+    'results',
+    'notes',
+    'dashboard',
 ]
+
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -115,3 +128,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+AUTH_USER_MODEL = "accounts.User"
