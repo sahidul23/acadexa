27c551dc0e75b5ba24400d4a206afe8a6090666e
sahidul islam <sahidafridi7099@gmail.com>
2026-06-20 14:59:32 +0530

    Complete React login and dashboard integration

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -61,9 +61,15 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.security.SecurityMiddleware",
 ]
 
 ROOT_URLCONF = 'config.urls'
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+]
 
 TEMPLATES = [
     {
