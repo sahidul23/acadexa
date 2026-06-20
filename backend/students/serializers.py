021df4161df114843d95ec6af148e8b36a6468f7
sahidul islam <sahidafridi7099@gmail.com>
2026-06-20 12:41:39 +0530

    Complete student API foundation

--- backend/students/serializers.py
+++ backend/students/serializers.py
@@ -1,17 +1,20 @@
-
 from rest_framework import serializers
 from .models import StudentProfile
 
 class StudentProfileSerializer(serializers.ModelSerializer):
+    student_name = serializers.CharField(source="user.username", read_only=True)
+    class_name = serializers.StringRelatedField(source="academic_class", read_only=True)
+    section_name = serializers.StringRelatedField(source="section", read_only=True)
+
     class Meta:
         model = StudentProfile
         fields = [
             "id",
-            "user",
+            "student_name",
             "admission_number",
             "roll_number",
-            "academic_class",
-            "section",
+            "class_name",
+            "section_name",
             "guardian_name",
             "guardian_phone",
         ]
