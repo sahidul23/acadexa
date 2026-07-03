7af1e314b3d3438ad98045c338d1dc52820971fe
sahidul islam <sahidafridi7099@gmail.com>
2026-07-03 22:47:14 +0530

    Add PostgreSQL packages

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -104,6 +104,13 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
 import dj_database_url
 
 DATABASES = {
@@ -111,8 +118,6 @@ DATABASES = {
         default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}"
     )
 }
-
-
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
 
