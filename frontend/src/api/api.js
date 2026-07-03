f9a1a86f968c0a2e11f4f1abc784e635ff68db5a
sahidul islam <sahidafridi7099@gmail.com>
2026-07-03 20:47:27 +0530

    Fix api.js

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,22 +1,8 @@
 import axios from "axios";
 
-import axios from "axios";
-
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
 });
-
-api.interceptors.request.use((config) => {
-  const token = localStorage.getItem("access");
-
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-
-  return config;
-});
-
-export default api;
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("access");
