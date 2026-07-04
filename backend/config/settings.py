68406b2423cc2661baf01c81af32f87ff19469cc
sahidul islam <sahidafridi7099@gmail.com>
2026-07-04 09:00:39 +0530

    Use Railway PostgreSQL environment variables

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -111,12 +111,17 @@ DATABASES = {
     }
 }
 
-import dj_database_url
+import os
 
 DATABASES = {
-    "default": dj_database_url.config(
-        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}"
-    )
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get("PGDATABASE"),
+        "USER": os.environ.get("PGUSER"),
+        "PASSWORD": os.environ.get("PGPASSWORD"),
+        "HOST": os.environ.get("PGHOST"),
+        "PORT": os.environ.get("PGPORT", "5432"),
+    }
 }
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
