090b74e3521632d9ca840f0c079b757c0593e65f
sahidul islam <sahidafridi7099@gmail.com>
2026-07-05 15:13:59 +0530

    Switch back to PostgreSQL

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -104,13 +104,18 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+import os
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("PGDATABASE"),
+        "USER": os.getenv("PGUSER"),
+        "PASSWORD": os.getenv("PGPASSWORD"),
+        "HOST": os.getenv("PGHOST"),
+        "PORT": os.getenv("PGPORT", "5432"),
     }
 }
-
 
 
 
